d378f3767cc68689a5a6d036e003d5b74cded363
Simon Friis Vindum <simonfv@gmail.com>
2025-09-14 12:48:13 +0200

    Apply shfmt to version changer script

--- docs/version_changer.sh
+++ docs/version_changer.sh
@@ -7,13 +7,13 @@ old_dir=$(pwd)
 # Get docs directoy the POSIX way
 DOCS_PATH="$0"
 if [ ! -e "$DOCS_PATH" ]; then
-  case $DOCS_PATH in
-    (*/*) exit 1;;
-    (*) DOCS_PATH=$(command -v -- "$DOCS_PATH") || exit;;
-  esac
+	case $DOCS_PATH in
+	*/*) exit 1 ;;
+	*) DOCS_PATH=$(command -v -- "$DOCS_PATH") || exit ;;
+	esac
 fi
 docs_dir=$(
-  cd -P -- "$(dirname -- "$DOCS_PATH")" && pwd -P
+	cd -P -- "$(dirname -- "$DOCS_PATH")" && pwd -P
 ) || exit
 
 # Go into docs dir
@@ -28,7 +28,8 @@ date=$(date '+%B %d, %Y')
 version=$(sh -c "${project_dir}/./projectdo --version" | grep -Eo '[0-9]\.[0-9]\.[0-9]+')
 
 printf '%s is the current projectdo version.\n' "$version"
-printf "What version do you want to change it to?\n: "; read -r ver_choice
+printf "What version do you want to change it to?\n: "
+read -r ver_choice
 
 sed -i "s/date\:.*/date: $date/g" "${man_dir}/projectdo.1.md"
 
@@ -41,9 +42,10 @@ printf 'Successfully updated version number to %s in package.json.\n' "$ver_choi
 sed -i "s/footer\:.*/footer: projectdo $ver_choice/" "${man_dir}/projectdo.1.md"
 printf 'Successfully updated version number to %s in projectdo.1.md\n' "$ver_choice"
 
-printf "Do you want to generate new manpage?\n[Y/n]: "; read -r man_choice
+printf "Do you want to generate new manpage?\n[Y/n]: "
+read -r man_choice
 if ! [ "$man_choice" = "n" ]; then
-    make manpage
+	make manpage
 fi
 
 # Go back to the dir where user was before
